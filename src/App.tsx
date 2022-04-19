5efb36344238ca7cb0e25ae6310c90c2cc130f0f
Jason Lee <jason.leevt@gmail.com>
2022-04-18 22:03:12 -0400

    New Change

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <h1>Hello Werld!</h1>
+        <h1>Something Different</h1>
         <a
           className="App-link"
           href="https://reactjs.org"

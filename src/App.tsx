9d01d1f008f9f81f55eeca874a6d3afa33dd29d9
Jason Lee <jason.leevt@gmail.com>
2022-05-10 21:25:22 -0400

    Created new folder, Erased preset code in App.tsx and replaced it with basic layout to start testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>Something Different</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Window'>
+        <header className='Window_Name'>Choice Window</header>
+      </div>
+      <div className='Window'>
+        <header className='Window_Name'>Result Window</header>
+      </div>
     </div>
   );
 }
